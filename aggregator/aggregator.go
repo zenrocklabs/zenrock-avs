15fa16327653199e3cdf7026abc6b11bd3ab52d7
Sasha Duke <sasha@zenrocklabs.io>
2024-11-21 21:19:24 +0000

    fix: set task cadence to 15 mins

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -29,7 +29,7 @@ const (
 	blockTimeSeconds         = 12 * time.Second
 	avsName                  = "zenrock"
 
-	taskCadence = 15 * time.Second
+	taskCadence = 15 * time.Minute
 )
 
 // Aggregator sends tasks (numbers to square) onchain, then listens for operator signed TaskResponses.
@@ -130,7 +130,7 @@ func (agg *Aggregator) Start(ctx context.Context) error {
 
 	// TODO(soubhik): refactor task generation/sending into a separate function that we can run as goroutine
 	ticker := time.NewTicker(taskCadence)
-	agg.logger.Infof("Aggregator set to send new task every 15 seconds...")
+	agg.logger.Infof("Aggregator set to send new task every 15 minutes...")
 	defer ticker.Stop()
 
 	// Initialize currentTaskId
